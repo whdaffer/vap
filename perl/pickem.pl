8a52f3385511c57c964dd6d57885d24dd1bc8a07
vapdev <>
2002-07-01 23:05:00 +0000

    Initial Revision

---
+++ perl/pickem.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+#
+# pickem.pl - CGI script for handling Tropical Storms archive
+#
+# Called from storms.html
+#
+# $Id$
+#
+# Modification Log:
+#
+# $Log$
+#
+use strict;
+use lib $ENV{VAP_SFTWR_PERL};
+use VapWWW2;
+
+pickem;
+exit;
+
