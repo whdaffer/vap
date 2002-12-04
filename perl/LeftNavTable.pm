7f3139a6905caab373686516e414ee02a2ddab6b
vapdev <>
2002-12-04 01:20:31 +0000

    Ongoing work

--- perl/LeftNavTable.pm
+++ perl/LeftNavTable.pm
@@ -1,6 +1,9 @@
 # $Id$
 #
 # $Log$
+# Revision 1.4  2002/12/03 05:57:14  vapdev
+# Ongoing work
+#
 # Revision 1.3  2002/12/02 17:24:07  vapdev
 # Moved to .../perl
 #
@@ -14,116 +17,360 @@
 #
 package LeftNavTable;
 use strict;
-use vars qw/$WESTPACIFIC $EASTPACIFIC $WESTATLANTIC %overlay_defs 
-	    $DEFUNCT_SATELLITES $DEFUNCT_VIEWS/;
-use lib $ENV{VAP_LIBRARY};
+use vars qw/$WESTPACIFIC $EASTPACIFIC $WESTATLANTIC $overlay_defs 
+	    @defunct_satellites $roi_hash $tsoo_defs /;
+use lib ($ENV{VAP_LIBRARY}, $ENV{VAP_SFTWR_PERL});
 use Carp;
 use HTML::Table;
 @LeftNavTable::ISA= qw/HTML::Table/;
+use CGI;
+use VapUtil;
 
 sub new {
 
   my $class = shift;
-  my $self=$class->SUPER::new(@_);
-  $DEFUNCT_VIEWS = "GOES.?(8|10)_?1_\\w+_\\d";
+  my $self=$class->SUPER::new(-rows=>8,-cols=>1);
 
-  # Get the defaults for the overlays from the overlay_defs file.
+  # Get the defaults for the various VAP products, as well as for the
+  # website itself
 
-  croak "Can't find overlay_defs!\n"
+  #website defaults:
+
+
+  croak "Can't find VapWebsite!\n"
+    unless (-e $ENV{VAP_LIBRARY}."/VapWebsite");
+  require "VapWebsite" || 
+    croak "Can't require ".$ENV{VAP_LIBRARY} . "VapWebsite\n";
+
+  # Overlay defaults
+  croak "Can't find overlay_defs_oo!\n"
     unless (-e $ENV{VAP_LIBRARY}."/overlay_defs_oo");
-
   require "overlay_defs_oo" || 
     croak "Can't require ".$ENV{VAP_LIBRARY} . "overlay_defs_oo\n";
 
-  # Get the defaults for the tropical storms processing 
-  # from the tropical_storm_defs file.
+  # Tropical storm defaults
+  croak "Can't find tropical_storm_defs_oo!\n" 
+    unless (-e $ENV{VAP_LIBRARY}."/tropical_storm_defs_oo");
+  require "tropical_storm_defs_oo" or 
+    croak "Can't require tropical_storm_defs_oo:$!\n";  
 
-  croak "Can't find tropical_storm_defs!\n" 
-    unless (-e $ENV{VAP_LIBRARY}."/tropical_storm_defs");
-  require "tropical_storm_defs" or croak "Can't require tropical_storm_defs:$!\n";
+  # Animation defaults.
 
+  my $roi_hash = auto_movie_defs();
 
-  # Get the keys from this file.
-  my @overlay_keys = keys %overlay_defs;
+  # Start building the tables within tables within tables within
+  # tables within tables . . . needed for this navbar.
 
-  # ====== West Atlantic, typically Goes 8 =================
+  # Some useful variables
 
-  my @regions = grep /GOES.?(8|EAST)/, @overlay_keys;
-  @regions = grep !/$DEFUNCT_VIEWS/,@regions;
+  my ($row, $col, $key, $value, $content, $link, $skip, $q, $webname);
 
-  my $WESTATLANTIC = {SATELLITE => ['GOES_8', 'GOES_EAST'],
-		      REGIONS => \@regions};
-  my $htmltable = HTML::Table->new(
-	      -rows=>scalar(@{$WESTATLANTIC->{REGIONS}})+1,
-				 -cols=>2,
-				 -align=>"left",
-				 -width=>"100\%",
-				 -spacing=>0,
-				 -padding=>0);
-  $WESTATLANTIC->{HTMLTABLE} = $htmltable;
+    # The outermost table, which will contain the entire navbar. Each
+    # of the subsequent tables will cells in this table.
 
-  @regions = grep /GOES.?(10|WEST)/,@overlay_keys;
-  @regions = grep !/$DEFUNCT_VIEWS/,@regions;
-
-  # ====== East Pacific, typically Goes 10 =================
-  my $EASTPACIFIC = {SATELLITE =>  ['GOES_10','GOES_WEST'],
-		      REGIONS => \@regions};
-
-  $htmltable = HTML::Table->new(
-	    -rows=>scalar(@{$EASTPACIFIC->{REGIONS}})+1,
-				-cols=>2,
-				-align=>"left",
-				-width=>"100\%",
-				-spacing=>0,
-				-padding=>0);
-  $EASTPACIFIC->{HTMLTABLE} = $htmltable;
+  # my $table1=HTML::Table->new(-rows=>8,-cols=>1);
 
 
 
-  # ====== West Pacific, typically GMS 5 =================
-  @regions = grep /GMS_?\d?/,@overlay_keys;
-  @regions = grep !/$DEFUNCT_VIEWS/,@regions;
 
-  my $WESTPACIFIC = {SATELLITE => ['GMS_5','GMS'],
-		     REGIONS => \@regions};
-
-  $htmltable = HTML::Table->new(
-	       -rows=>scalar(@{$WESTPACIFIC->{REGIONS}})+1,
-			      -cols=>2,
-			      -align=>"left",
-			      -width=>"100\%",
-			      -spacing=>0,
-			      -padding=>0);
-
-  $WESTPACIFIC->{HTMLTABLE} = $htmltable;
-
-
-  # ========= Tropical Storms ===========================
-
-  @regions = grep !/$DEFUNCT_VIEWS/,@overlay_keys;
-  my $TROPICALSTORMS = {SATELLITE => ['GOES_10', 'GOES_8', 'GOES_EAST',
-				       'GOES_WEST', 'GMS_5', 'GMS'],
-		       REGIONS => \@regions};
+#=-*=-*=-*=-*=-*=-*=-*=-*=-* Overlays =-*=-*=-*=-*=-*=-*=-*=-*=-*=-*=-*=-*
 
 
 
-  # Set the titles
-  $WESTATLANTIC->{HTMLTABLE}->setCell(1,1,"West Atlantic",-colspan=>2);
-  $EASTPACIFIC->{HTMLTABLE}->setCell(1,1,"East Pacific",-colspan=>2);
-  $WESTPACIFIC->{HTMLTABLE}->setCell(1,1,"West Pacific",-colspan=>2);
+
+    # Construct the table that will hold the overlay part of the nav
+    # bar. This goes in cell 1,1
+
+  my $overlay_table = HTML::Table->new(-rows=>2,-cols=>1);
+  $overlay_table->setCaption("Cloud Overlays",'TOP');
+
+     # The actual navigation links are constructed in these two tables
+     # and will get the information on which 'regions' to use from
+     # $VAP_LIBRARY/overlay_defs_oo (the WEB=>...) portion of that
+     # hash.
+
+    # ========= SeaWinds on ADEOS-II table ============
 
 
-#   my $maintable = HTML::Table->new(-rows=>6,
-# 			   -cols=>1,
-# 			   -align=>"left",
-# 			   -width=>"40\%",
-# 			   -border=>1);
+  my $overlay_sw_table=HTML::Table->new(-rows=>8,-col=>1,-align=>'RIGHT');
+  $overlay_sw_table->setCaption("SeaWinds",'TOP');
+  $row=0;
+  while ( ($key, $value) = each %{$overlay_defs}) {
+      # value here is a reference to a hash, see
+      # $VAP_LIBRARY/overlay_defs_oo for its definition.
+    $skip=0;
+    if (@defunct_satellites) {
+      foreach my $s (@defunct_satellites){
+	$skip = grep (/$s/i, $key);
+	last if $skip
+      }
+    }
+    next if $skip;
+    next if !$value->{WEB}->{ACTIVE};
 
-  $self->{WESTATLANTIC} = $WESTATLANTIC;
-  $self->{EASTPACIFIC} = $EASTPACIFIC ;
-  $self->{WESTPACIFIC} = $WESTPACIFIC;
-#	   MAINTABLE => $maintable,
-#	     @_};
+    $row++;
+    $q=CGI->new(-no_debug=>1);
+    $link = $q->a({-href=>"overlay_sw.html#$key"},
+		  $value->{WEB}->{NAME} );
+    $content=$q->p({-align=>'LEFT'},$link);
+    $overlay_sw_table->setCell($row, 1, $content);
+  }
+
+
+    # ========= SeaWinds on QuikSCAT table ================
+
+
+  my $overlay_qs_table=HTML::Table->new(-rows=>8,-col=>1,-align=>'RIGHT');
+  $overlay_qs_table->setCaption("QuikSCAT",'TOP');
+  $row=0;
+  while ( ($key, $value) = each %{$overlay_defs}) {
+    $skip=0;
+    if (@defunct_satellites) {
+      foreach my $s (@defunct_satellites){
+	$skip = grep (/$s/i, $key);
+	last if $skip
+      }
+    }
+    next if $skip;
+    next if !$value->{WEB}->{ACTIVE};
+    $row++;
+    $q=CGI->new(-no_debug=>1);
+    $link = $q->a({-href=>"overlay_qs.html#$key"},
+		  $value->{WEB}->{NAME});
+    $content=$q->p({-align=>'LEFT'},$link);
+    $overlay_qs_table->setCell($row, 1, $content);
+  }
+
+
+    # Now put the two tables into the overall `overlay' cell of the
+    # Left Nav table
+
+  $overlay_table->setCell(1,1,$overlay_sw_table);
+  $overlay_table->setCell(2,1,$overlay_qs_table);
+
+    # And put the overlay table into outermost table, self
+  $self->setCell(1,1,$overlay_table);
+
+
+
+
+#=-*=-*=-*=-*=-*=-*=-*=-*  Animations    *=-*=-*=-*=-*=-*=-*=-*=-*=-*=-*=-*=-*=-*=-*
+
+
+
+
+    # Construct the table that will hold the animation part of the nav
+    # bar. This goes in cell 2,1. The animations won't split QS from
+    # SW, so there's no need for the additional layer of tables as is
+    # the case with the overlays.
+
+  my $anim_table = HTML::Table->new(-rows=>9,-cols=>1);
+  $anim_table->setCaption("Animations",'TOP');
+  $row=0;
+  while (($key, $value) = each %{$roi_hash}){
+    $skip=0;
+    if (@defunct_satellites) {
+      foreach my $s (@defunct_satellites){
+	$skip = grep (/$s/i, $key);
+	last if $skip
+      }
+    }
+    next if $skip;
+    $row++;
+    $q=CGI->new(-no_debug=>1);
+    $link = $q->a({-href=>"anim.html#$key"},$value);
+    $content=$q->p({-align=>'LEFT'},$link);
+    $anim_table->setCell($row, 1, $content);
+  }
+
+
+    # Now put the anim_table into self
+
+  $self->setCell(2,1,$anim_table); 
+
+#=-*=-*=-*=-*=-*=-*=-*=-*  Tropical Storms *=-*=-*=-*=-*=-*=-*=-*=-*=-*=-*=-*=-*
+
+
+
+
+    # Construct the table that will hold the Tropical Storms. Cell
+    # 3,1. this will get the information need to construct itself from
+    # the tropical_storm_defs_oo hash.
+
+
+  my $ts_table = HTML::Table->new(-caption=>"Tropical Storms", -rows=>2,-col=>1);
+  $ts_table->setCaption("Tropical Storms",'TOP');
+    # ============= Seawinds on ADEOS-II =================
+
+  my $ts_sw_table=HTML::Table->new(-rows=>8,-col=>1,-align=>'RIGHT');
+  $ts_sw_table->setCaption("SeaWinds",'TOP');
+  $row=0;
+
+  foreach my $sat (@{$tsoo_defs->{SATELLITES}}) {
+    $skip=0;
+    if (@defunct_satellites) {
+      foreach my $s (@defunct_satellites){
+	$skip = grep (/$s/i, $sat);
+	last if $skip
+      }
+    }
+    next if $skip;
+    $row++;
+    $q=CGI->new(-no_debug=>1);
+    $webname = $tsoo_defs->{WEB}->{NAME}->{$sat};
+    $link = $q->a({-href=>"ts_sw.html#$sat"},
+		  "$webname");
+    $content=$q->p({-align=>'LEFT'},$link);
+    $ts_sw_table->setCell($row, 1, $content);
+  }
+
+
+    # ============= Seawinds on QuikSCAT  =================
+  my $ts_qs_table=HTML::Table->new(-rows=>8,-col=>1,-align=>'RIGHT');
+  $ts_qs_table->setCaption("QuikSCAT",'TOP');
+  $row=0;
+  foreach my $sat (@{$tsoo_defs->{SATELLITES}}) {
+    $skip=0;
+    if (@defunct_satellites) {
+      foreach my $s (@defunct_satellites){
+	$skip = grep (/$s/i, $sat);
+	last if $skip
+      }
+    }
+    next if $skip;
+    $row++;
+    $q=CGI->new(-no_debug=>1);
+    $webname = $tsoo_defs->{WEB}->{NAME}->{$sat};
+    $link = $q->a({-href=>"ts_qs.html#$sat"},
+		  "$webname");
+    $content=$q->p({-align=>'LEFT'},$link);
+    $ts_qs_table->setCell($row, 1, $content);
+  }
+
+    # Now put these two tables into the overlay Tropical Storm cell
+    # for the Nav Bar
+
+  $ts_table->setCell(1,1,$ts_sw_table);
+  $ts_table->setCell(2,1,$ts_qs_table);
+
+    # And put the Tropical storm table into the proper slot in the
+    # navbar.
+
+  $self->setCell(3,1,$ts_table);
+
+
+#=-*=-*=-*=-*=-* Now, for the rest of the NavBar =-*=-*=-*=-*=-*=-*=-*=-*
+
+
+    # Cell 4,1 goes the 'Status' page.
+  $q=CGI->new(-no_debug=>1);
+  $link=$q->a({-href=>"status.html"},"Status");
+  $content=$q->p({-align=>"RIGHT"},$link);
+  $self->setCell(4,1,$content);
+
+    # Cell 5,1 goes to 'Specials', if any.
+  $q=CGI->new(-no_debug=>1);
+  $link=$q->a({-href=>"special.html"},"Special Products");
+  $content=$q->p({-align=>"RIGHT"},$link);
+  $self->setCell(5,1,$content);
+
+    # Cell 6,1 is for "information" about the website, 
+  $q=CGI->new(-no_debug=>1);
+  $link=$q->a({-href=>"info.html"},"Information");
+  $content=$q->p({-align=>"RIGHT"},$link);
+  $self->setCell(6,1,$content);
+
+    # Cell 7,1 is the mailto url.
+  $q=CGI->new(-no_debug=>1);
+  $link=$q->a({-href=>"mailto: webmaster\@haifung.jpl.nasa.gov"},"Contact us!");
+  $content=$q->p({-align=>"RIGHT"},$link);
+  $self->setCell(7,1,$content);
+
+  
+#   # Get the keys from the overlay file.
+#   my @overlay_keys = keys %{$overlay_defs};
+
+#   # ====== West Atlantic, typically Goes 8 =================
+
+#   my @regions = grep /GOES.?(8|EAST)/, @overlay_keys;
+#   @regions = grep !/$DEFUNCT_VIEWS/,@regions;
+
+#   my $WESTATLANTIC = {SATELLITE => ['GOES_8', 'GOES_EAST'],
+# 		      REGIONS => \@regions};
+#   my $htmltable = HTML::Table->new(
+# 	      -rows=>scalar(@{$WESTATLANTIC->{REGIONS}})+1,
+# 				 -cols=>2,
+# 				 -align=>"left",
+# 				 -width=>"100\%",
+# 				 -spacing=>0,
+# 				 -padding=>0);
+#   $htmltable->setCaption("
+#   $WESTATLANTIC->{HTMLTABLE} = $htmltable;
+
+#   @regions = grep /GOES.?(10|WEST)/,@overlay_keys;
+#   @regions = grep !/$DEFUNCT_VIEWS/,@regions;
+
+#   # ====== East Pacific, typically Goes 10 =================
+#   my $EASTPACIFIC = {SATELLITE =>  ['GOES_10','GOES_WEST'],
+# 		      REGIONS => \@regions};
+
+#   $htmltable = HTML::Table->new(
+# 	    -rows=>scalar(@{$EASTPACIFIC->{REGIONS}})+1,
+# 				-cols=>2,
+# 				-align=>"left",
+# 				-width=>"100\%",
+# 				-spacing=>0,
+# 				-padding=>0);
+#   $EASTPACIFIC->{HTMLTABLE} = $htmltable;
+
+
+
+#   # ====== West Pacific, typically GMS 5 =================
+#   @regions = grep /GMS_?\d?/,@overlay_keys;
+#   @regions = grep !/$DEFUNCT_VIEWS/,@regions;
+
+#   my $WESTPACIFIC = {SATELLITE => ['GMS_5','GMS'],
+# 		     REGIONS => \@regions};
+
+#   $htmltable = HTML::Table->new(
+# 	       -rows=>scalar(@{$WESTPACIFIC->{REGIONS}})+1,
+# 			      -cols=>2,
+# 			      -align=>"left",
+# 			      -width=>"100\%",
+# 			      -spacing=>0,
+# 			      -padding=>0);
+
+#   $WESTPACIFIC->{HTMLTABLE} = $htmltable;
+
+
+#   # ========= Tropical Storms ===========================
+
+#   @regions = grep !/$DEFUNCT_VIEWS/,@overlay_keys;
+#   my $TROPICALSTORMS = {SATELLITE => ['GOES_10', 'GOES_8', 'GOES_EAST',
+# 				       'GOES_WEST', 'GMS_5', 'GMS'],
+# 		       REGIONS => \@regions};
+
+
+
+#   # Set the titles
+#   $WESTATLANTIC->{HTMLTABLE}->setCell(1,1,"West Atlantic",-colspan=>2);
+#   $EASTPACIFIC->{HTMLTABLE}->setCell(1,1,"East Pacific",-colspan=>2);
+#   $WESTPACIFIC->{HTMLTABLE}->setCell(1,1,"West Pacific",-colspan=>2);
+
+
+# #   my $maintable = HTML::Table->new(-rows=>6,
+# # 			   -cols=>1,
+# # 			   -align=>"left",
+# # 			   -width=>"40\%",
+# # 			   -border=>1);
+
+#   $self->{WESTATLANTIC} = $WESTATLANTIC;
+#   $self->{EASTPACIFIC} = $EASTPACIFIC ;
+#   $self->{WESTPACIFIC} = $WESTPACIFIC;
+# #	   MAINTABLE => $maintable,
+# #	     @_};
+
+
   return bless $self, ref($class) || $class;
 }
 
--- perl/vap-website.pl
+++ perl/vap-website.pl
@@ -1,5 +1,12 @@
 #!/usr/bin/perl -w
 #
+# $Id$
+#
+# Modifications:
+#
+# $Log$
+#
+#
 use strict;
 use CGI;
 use HTML::Table;
@@ -8,47 +15,81 @@ use LeftNavTable;
 my $q=CGI->new(-nodebug=>1);
 my $body=CGI->new(-nodebug=>1);
 
-my $html=$q->start_html(-title=>"Haifung Winds stuff",
-			-meta=>{"Keywords" =>
+my $html=$q->start_html(
+	    -title=>"Haifung Winds stuff",
+	    -meta=>{"Keywords" =>
 				qw/Radar Scatterometry SeaWinds 
-                                    Winds Oceanography/});
+				   Winds Oceanography/},
+	     -background=>"file:///disk2/vap/www/htodcs/images/fuji7.gif",
+	     -style=>{
+		      -code=>'A:link {text-decoration:none;},A:visited {text-decoration:none;}'
+		     },
+	     -link=>"336699", -alink=>"003366",-vlink=>"336699",-marginheight=>"0",
+	     -topmargin=>"0", -leftmargin=>"0", -onload=>"init()", -onresize=>"redo()",
+	     -script=>{-language=>"javascript", -code=>"var _OLD_ONERROR = window.onerror; window.onerror=null"},
+	     -script=> {-language=>"javascript", -src=>"file:///usr/people/vapdev/development/vap/html/js/ua.js"},
+	     -script=> {-language=>"javascript1.2", -src=>"file:///usr/people/vapdev/development/vap/html/js/base.js"},
+	     -script=> {-language=>"javascript1.2", -src=>"file:///usr/people/vapdev/development/vap/html/js/breadcrumbs.js"},
+	     -script=> {-language=>"javascript",
+			-code=>"
+                           if (navigator.version < 5)
+                                 window.onerror = _OLD_ONERROR = defaultOnError;
+                           else
+                              window.onerror = _OLD_ONERROR;
+                           "},
+	     -script=>{-language=>"JavaScript", 
+		       -code=>"function blockError(){return true;}
+                              window.onerror = blockError;"},
+	     -script => { -language=>"JavaScript1.2", -src=>"file:///usr/people/vapdev/development/vap/html/js/xbStyle.js"},
+	     -script => { -language=>"JavaScript1.2", -src=>"file:///usr/people/vapdev/development/vap/html/js/xbCollapsibleLists.js"},
+             -script => { -language=>"JavaScript1.2", -src=>"file:///usr/people/vapdev/development/vap/html/js/viewSource.js"},
+	     -script => { -language=>"JavaScript", 
+               -code=> "function init() { return true; }"} 
+);
 
-  # The outer most table. All content is contained in this table
+
+
+
+
+
+
+  # Outermost table, contains the entire content of the page.
+
 my $outsidetable = HTML::Table->new(-rows=>3,
 				    -cols=>2,
 				    -align=>"left",
 				    -width=>"100\%",
 				   -border=>1);
+  # For the top navbar
 
-  # Contains the top navigation bar.
 my $topbody = HTML::Table->new(-rows=>1,
 			       -cols=>3);
 
-  # The left nav table
-my $leftnavtable = LeftNavTable->new(-rows=>6,
-				    -cols=>1);
+  # For the left navbar
+my $leftnavtable = LeftNavTable->new();
 
-my @navcol = ($q->a({-href=>"#ATLANTIC"},"Atlantic"),
-	      $q->a({-href=>"#EASTPAC"},"East Pacific"),
-	      $q->a({-href=>"#WESTPAC"},"West Pacific"));
 
-$leftnavtable -> addCol(@navcol);
+# my @navcol = ($q->a({-href=>"#ATLANTIC"},"Atlantic"),
+# 	      $q->a({-href=>"#EASTPAC"},"East Pacific"),
+# 	      $q->a({-href=>"#WESTPAC"},"West Pacific"));
 
+# $leftnavtable -> addCol(@navcol);
+
+  # The table in the middle of the page.
 my $mainbodytable = HTML::Table->new(-rows=>6,
 				      -cols=>1,
 				      -width=>"60\%",
  				      -border=>1);
+
 my $bodyhtml = $body->h1("Welcome to my test page");
-$bodyhtml .= $body->a(-href=>"http://haifung.jpl.nasa.gov",
-		      -name=>"#HAIFUNG",
+$bodyhtml .= $body->a({-href=>"http://haifung.jpl.nasa.gov",
+		      -name=>"#HAIFUNG"},
 		      "Haifung web site");
-$bodyhtml .= $body->p();
-$bodyhtml .= "Just some more test to check out the formatting";
-$bodyhtml .= $body->img(-src=>
-			"file://" . $ENV{'VAP_WWW_TOP'} . 
-			"/images/fuji.gif",
-			-alt=>"Fuji",
-			-align=>"left");
+$bodyhtml .= $body->p("Just some more test to check out the formatting");
+#$bodyhtml .= $body->img({-src=>
+#			"file:///disk2/vap/www/htdocs/images/fuji7.gif",
+#			-alt=>"Fuji",
+#			-align=>"left"});
 
 $mainbodytable->setCell(1,1,$bodyhtml);
 
